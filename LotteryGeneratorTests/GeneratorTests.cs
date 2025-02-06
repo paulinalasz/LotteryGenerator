36fc43083d1ebcb5261270e49ccc99b8cb933dce
laszpaulina <48447681+laszpaulina@users.noreply.github.com>
2025-02-06 13:28:08 +0800

    Create first unit test EachNumberMustBeUniqueInSequence

--- LotteryGeneratorTests/GeneratorTests.cs
+++ LotteryGeneratorTests/GeneratorTests.cs
@@ -9,8 +9,33 @@ namespace LotteryGeneratorTests
         [TestMethod]
         public void EachNumberMustBeUniqueInSequence()
         {
-            var mockRandom = new Mock<IRandomNumberGenerator>();
-            //var generator = new Generator();
+            // Arrange
+            var randomMock = new Mock<IRandomNumberGenerator>();
+            randomMock.SetupSequence(r => r.Next(It.IsAny<int>(), It.IsAny<int>()))
+                .Returns(1)  
+                .Returns(2)
+                .Returns(3)  
+                .Returns(4)
+                .Returns(5) 
+                .Returns(6);
+
+            var generator = new Generator(randomMock.Object);
+
+            // Act
+            var generatedNumbers = generator.Generate();
+
+            // Assert
+            var generatedNumbersAsList = new List<int>
+            { 
+                generatedNumbers.Number1,
+                generatedNumbers.Number2,
+                generatedNumbers.Number3,
+                generatedNumbers.Number4,
+                generatedNumbers.Number5,
+                generatedNumbers.BonusNumber
+            };
+
+            Assert.AreEqual(generatedNumbersAsList.Count == generatedNumbersAsList.Distinct().Count(), true);
         }
     }
 }
