1115ed50f88dbbd3fe2b7b556fe2ad73b3f8affe
laszpaulina <48447681+laszpaulina@users.noreply.github.com>
2025-02-06 14:13:43 +0800

    Make test use dynamic data so that we can set a different amount of random inputs for each test. Ensure that test never gets stuck generating the same random number

--- LotteryGeneratorTests/GeneratorTests.cs
+++ LotteryGeneratorTests/GeneratorTests.cs
@@ -6,18 +6,18 @@ namespace LotteryGeneratorTests
     [TestClass]
     public sealed class GeneratorTests
     {
-        [TestMethod]
-        public void EachNumberMustBeUniqueInSequence()
+        private readonly Random _random = new Random();
+
+        [DataTestMethod]
+        [DynamicData(nameof(NumberSequences), DynamicDataSourceType.Method)]
+        public void EachNumberMustBeUniqueInSequence(Queue<int> randomNumberSequence)
         {
             // Arrange
             var randomMock = new Mock<IRandomNumberGenerator>();
-            randomMock.SetupSequence(r => r.Next(It.IsAny<int>(), It.IsAny<int>()))
-                .Returns(1)  
-                .Returns(2)
-                .Returns(3)  
-                .Returns(4)
-                .Returns(5) 
-                .Returns(6);
+            // This ensures that the test will never loop forever, as after completing our handwritten random sequence
+            // we return to generating truely random numbers
+            randomMock.Setup(r => r.Next(It.IsAny<int>(), It.IsAny<int>()))
+                      .Returns(() => randomNumberSequence.Count > 0 ? randomNumberSequence.Dequeue() : _random.Next(1, 60));
 
             var generator = new Generator(randomMock.Object);
 
@@ -26,7 +26,7 @@ namespace LotteryGeneratorTests
 
             // Assert
             var generatedNumbersAsList = new List<int>
-            { 
+            {
                 generatedNumbers.Number1,
                 generatedNumbers.Number2,
                 generatedNumbers.Number3,
@@ -35,7 +35,17 @@ namespace LotteryGeneratorTests
                 generatedNumbers.BonusNumber
             };
 
-            Assert.AreEqual(generatedNumbersAsList.Count == generatedNumbersAsList.Distinct().Count(), true);
+            Assert.IsTrue(generatedNumbersAsList.Count == generatedNumbersAsList.Distinct().Count());
+        }
+
+        public static IEnumerable<object[]> NumberSequences()
+        {
+            // Example data sets for different test cases
+            yield return new object[] { new Queue<int>(new[] { 55, 21, 12, 16, 15, 59 }) }; // Expected non-issue input 
+            yield return new object[] { new Queue<int>(new[] { 1, 2, 3, 4, 5, 6 }) };
+            yield return new object[] { new Queue<int>(new[] { 1, 1, 2, 3, 4, 5, 6 }) };
+            yield return new object[] { new Queue<int>(new[] { 1, 2, 3, 4, 5, 2 }) }; // First 5 numbers are unique, but the bonus is the same
+            yield return new object[] { new Queue<int>(new[] { 1, 1, 1, 1, 1, 1 }) }; // First few numbers are all the same
         }
     }
 }
