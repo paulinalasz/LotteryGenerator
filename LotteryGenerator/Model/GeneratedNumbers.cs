cbb6762fb81f41583ce063ba7cc98a13ab8fc160
laszpaulina <48447681+laszpaulina@users.noreply.github.com>
2025-02-06 11:14:40 +0800

    Create the GeneratedNumbers model as well as the Generator class that creates our lottery numbers

---
+++ LotteryGenerator/Model/GeneratedNumbers.cs
@@ -0,0 +1,27 @@
+﻿namespace LotteryGenerator.Model
+{
+    public sealed class GeneratedNumbers
+    {
+        public GeneratedNumbers(int number1,
+            int number2,
+            int number3,
+            int number4,
+            int number5,
+            int bonusNumber) 
+        {
+            Number1 = number1;
+            Number2 = number2;
+            Number3 = number3;
+            Number4 = number4;
+            Number5 = number5;
+            BonusNumber = bonusNumber;
+        }
+
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+        public int Number3 { get; set; }
+        public int Number4 { get; set; }
+        public int Number5 { get; set; }
+        public int BonusNumber { get; set; }    
+    }
+}
